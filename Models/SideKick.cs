30bd3f2f8409a6f959cd19ce8d0cb86889fba841
anthonyb <200564553@student.georgianc.on.ca>
2026-01-22 18:39:34 -0500

    I have adde this model

---
+++ Models/SideKick.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SidekickRegistry.Models
+{
+    public class Sidekick
+    {
+        [Key]
+        public int SidekickId { get; set; }
+
+        [Required]
+        public required string Alias { get; set; }
+
+        public required string SpecialPower { get; set; }
+
+        public int YearsActive { get; set; }
+
+        // Foreign Key
+        public int SuperheroId { get; set; }
+
+        // Navigation Property
+        [ForeignKey("SuperheroId")]
+        public required Superhero Superhero { get; set; }
+    }
+}
---
+++ Models/SuperHero_.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace SidekickRegistry.Models
+{
+    public class Superhero
+    {
+        [Key]
+        public int SuperheroId { get; set; }
+
+        [Required]
+        public required string Alias { get; set; }
+
+        public required string RealName { get; set; }
+
+        public required string OriginStory { get; set; }
+
+        // Navigation Property
+        public required ICollection<Sidekick> Sidekicks { get; set; }
+    }
+ 
+    }
